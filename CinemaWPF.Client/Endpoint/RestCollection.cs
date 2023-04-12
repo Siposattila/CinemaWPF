4ac7146d996b7f9f4f857d7b8b0ab89fbf49f1c9
Tolnai Gergő <gergotolnai001@gmail.com>
2023-04-12 09:11:06 +0200

    namespace fixfor RestCollection

--- CinemaWPF.Client/Endpoint/RestCollection.cs
+++ CinemaWPF.Client/Endpoint/RestCollection.cs
@@ -11,7 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace MovieDbApp.RestClient
+namespace CinemaWPF.Client.RestCollection
 {
     public class RestService
     {
--- CinemaWPF.Client/ViewModels/MainWindowViewModel.cs
+++ CinemaWPF.Client/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CinemaWPF.Client.RestCollection;
 
 namespace CinemaWPF.Client.ViewModels
 {
